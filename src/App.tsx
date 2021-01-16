72132cb1807acc2c97ae4d03c045ec11b3778d01
sunyill <sunyill0707@163.com>
2021-01-16 14:45:22 +0800

    添加字體庫

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-// import './App.css';
 import robot from './robotsData/robots.json'
 import RobotData from './components/Robot'
+import svgHeader from './assets/images/logo.svg'
 import styles from './App.module.css'
+
 
 function App() {
   return (
     <div className={styles.app}>
+      <header className = {styles.appHeader}>
+        <img src={svgHeader} className={styles.appLogo} alt=""/>
+        <h1>Robot完美小机器人模型堪比变形金刚</h1>
+      </header>
       <div className={styles.robotList}>
         {robot.map((rEle) => <RobotData id={rEle.id} name={rEle.name} email={rEle.email} />)}
       </div>
