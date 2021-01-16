7a7d63fdb31adc6453f975b92a9625912cae33d0
sunyill <sunyill0707@163.com>
2021-01-16 20:55:43 +0800

    reactEvent处理

--- src/components/ShoppingCar.tsx
+++ src/components/ShoppingCar.tsx
@@ -1,24 +1,74 @@
 import React from "react";
 import styles from "./ShoppingCar.module.css";
+import { FiShoppingCart } from "react-icons/fi";
 
 interface Props {}
 
 interface State {
   isOpen: boolean;
+  count: number;
 }
 
 class ShoppingCar extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      count: 1
     };
+    // this.handleClick = this.handleClick.bind(this)
   }
-
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("e.target", e.target);
+    console.log("e.currentTarget", e.currentTarget);
+    if ((e.target as HTMLElement).nodeName === "SAPN") {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+    // this.setState({
+    //   count: this.state.count + 1
+    // });
+  };
+  // handleClick(e) {
+  //   this.setState({ isOpen: !this.state.isOpen });
+  // }
   render() {
     return (
       <div className={styles.cartContainer}>
-        <button className={styles.button} onClick={()=>{this.setState({isOpen:!this.state.isOpen})}}>购物车 2件</button>
+        <div>
+          <button
+            className={styles.button}
+            // onClick={(preState,preProps)=>{
+            //   return {count:preState.count+1}
+            // },()=>{
+            //   console.log(this.state.count);
+            // }}
+            onClick={() => {
+              this.setState(
+                (preState, preProps) => {
+                  return { count: preState.count + 1 };
+                },
+                () => {
+                  console.log(this.state.count);
+                }
+              );
+              this.setState(
+                (preState, preProps) => {
+                  return { count: preState.count + 1 };
+                },
+                () => {
+                  console.log(this.state.count);
+                }
+              );
+            }}
+          >
+            点击增加
+          </button>
+          <span>{this.state.count} 次数</span>
+          <button className={styles.button} onClick={this.handleClick}>
+            <FiShoppingCart />
+            <span>购物车 2件</span>
+          </button>
+        </div>
         <div
           className={styles.cartDropDown}
           style={{ display: this.state.isOpen ? "block" : "none" }}
